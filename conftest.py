b11de9a943a6f2f47ec239e0aab30d6216061860
Lipatnikova <106307423+Lipatnikova@users.noreply.github.com>
2022-11-01 17:51:42 +0400

    Update conftest.py
    add Firefox browser
    
    Co-Authored-By: Lipatnikova <106307423+lipatnikova@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -12,14 +12,42 @@
 
 # 3_6_2
 
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
+
+# 3_6_6
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    # options = Options()
+    # options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
+    # browser = webdriver.Chrome(options=options)
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
